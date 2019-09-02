2803d0806c2f2c49cb21a167ffe369249cda5e1c
erlangga <angga@cls-indo.com>
2019-09-03 00:36:13 +0700

    Create user & product controller

---
+++ application/controllers/User.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+		$this->load->view('layouts/template');
+    }
+    
+	public function a2()
+	{
+		echo "22222";
+		// $this->load->view('welcome_message');
+	}
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,12 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// $this->load->view('welcome_message');
+		$this->load->view('layouts/template');
+	}
+	public function a2()
+	{
+		echo "22222";
+		// $this->load->view('welcome_message');
 	}
 }
