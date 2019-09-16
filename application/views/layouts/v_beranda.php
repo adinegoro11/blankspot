554a814fb28a4dac5c65bf0925b5d1b1a586889e
erlangga <angga@cls-indo.com>
2019-09-17 06:00:30 +0700

    Add telkom_property image

--- application/views/layouts/v_beranda.php
+++ application/views/layouts/v_beranda.php
@@ -1,6 +1,7 @@
 <div class="panel panel-primary">
     <div class="panel-heading"> <h3 class="panel-title">Home</h3> </div>
     <div class="panel-body">
+        <img src="<?= base_url()?>assets/images/telkom_property.jpg" alt="..." class="img-thumbnail">
         <h1><?=$query_judul->value?></h1>
         <p class="lead"><?=$query_kalimat->value?></p>
     </div>
